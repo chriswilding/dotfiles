6de73bf99ba4af9892d600de86e789478ea25c21
Chris Wilding <hello@chriswilding.co.uk>
2016-02-27 19:32:23 +0000

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+ln -s .gemrc ~/.gemrc
+ln -s .gitconfig ~/.gitconfig
+ln -s .gitignore_global ~/.gitignore_global
+ln -s .rspec ~/.rspec
+ln -s .tmux.conf ~/.tmuc.conf
+ln -s .vimrc ~/.vimrc
+ln -s .zshrc ~/.zshrc
