523f03dc700f5e0589dbfec2e9615d33958dc7f9
Chris Wilding <hello@chriswilding.co.uk>
2016-05-12 21:47:52 +0100

    Add initial emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+;; Packages
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar packages
+  '(
+    ))
+
+(dolist (p packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
